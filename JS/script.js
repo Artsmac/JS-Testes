326b9aa734dbe7c13f30ebc591577a34af227047
Arthur Maciel <arthurmaciel320@gmail.com>
2025-10-09 21:44:44 -0300

    objetos-teste

--- JS/script.js
+++ JS/script.js
@@ -177,4 +177,59 @@ function jaVisitei(paisesVisitados) {
 precisoVisitar(20);
 jaVisitei(20);
 
+//objetos
 
+/*var pessoa = {
+    nome:'Arhtur',
+    sobrenome:'Maciel',
+    idade: 25,
+};
+console.log(pessoa['nome']);
+*/
+var quadrado = {
+    lados: 4,
+    area: function(lado) {
+        return lado * lado;
+    }, perimetro: function(lado) {
+        return lado+lado+lado+lado;
+    }
+}
+console.log(quadrado.area(3));
+console.log(quadrado.perimetro(8));
+
+// Crie um objeto com os seus dados pessoais
+// Deve possui pelo menos duas propriedades nome e sobrenome
+var dados = {
+    nome:'Arthur',
+    sobrenome: 'Maciel',
+    idade:25,
+    profissao:'TI',
+    possuiFaculdade: false,
+}
+// Crie um método no objeto anterior, que mostre o seu nome completo
+dados.nomeCompleto = function() {
+    return `${this.nome} ${this.sobrenome}`;
+}
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+  preco: 1000,
+  portas: 4,
+  marca: 'Audi',
+}
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+var cachorro = {
+    raca: 'Labrador',
+    cor: 'preto',
+    idade: 10,
+    latir(pessoa) {
+        if (pessoa === 'homem') {
+            return 'Latir';
+        } else {
+            return 'Não late';
+        }
+    }
+}
